952720eecb3009212b472d8bece6e3b6f01c3009
Rajender216 <216rajender@gmail.com>
2025-05-18 20:28:20 +0530

    first commit

--- backend/controllers/blogControllers.js
+++ backend/controllers/blogControllers.js
@@ -2,5 +2,3 @@ const saveDraft = (req, res) => {}
 const publishBlog = (req, res) => {}
 const getAllBlogs = (req, res) => {}
 const getBlogById = (req, res) => {}
-
-export { saveDraft, publishBlog, getAllBlogs, getBlogById }
