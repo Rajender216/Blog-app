1e0d88f8838dfd339da18422b604d21fac4295c6
Rajender216 <216rajender@gmail.com>
2025-05-19 16:56:51 +0530

    vercel.json added

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,28 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from './config/mongodb.js'
-import connectCloudinary from './config/cloudinary.js'
-import userRouter from './routes/userRoutes.js'
-import blogRouter from './routes/blogRoutes.js'
-
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoutes.js";
+import blogRouter from "./routes/blogRoutes.js";
 
 // app config
-const app = express()
-const port = process.env.PORT || 4000
-connectDB()
-connectCloudinary()
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
 
 // middlewares
-app.use(express.json())
-app.use(cors({credentials: true, origin: '*'}))
-app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json());
+app.use(cors({ credentials: true, origin: "*" }));
+app.use(express.urlencoded({ extended: false }));
 
 // API endpoints
-app.use('/api/blogs', blogRouter)
-app.use('/api/user', userRouter)
+app.use("/api/blogs", blogRouter);
+app.use("/api/user", userRouter);
 
-app.get('/', (req, res) => {
-    res.send('API WORKING fine')
-})
+app.get("/", (req, res) => {
+  res.send("API WORKING fine");
+});
 
-app.listen(port, () => console.log("server started", port))
+app.listen(port, () => console.log("server started", port));
